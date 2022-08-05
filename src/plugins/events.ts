76450eb68b75fc17ae0a710daafd6b505b56ed64
Ludwig Frank <mail@ludwigfrank.com>
2022-08-06 00:49:59 +0200

    update - with _ in events

--- src/plugins/events.ts
+++ src/plugins/events.ts
@@ -16,7 +16,7 @@ function onTrackEvent(e: CustomEvent<TrackEventParams>) {
   if (event && window.webkit) {
     try {
       window.webkit.messageHandlers.track.postMessage({
-        event: event,
+        event: event.replaceAll('-', '_'),
         properties: properties ? properties : {},
       })
     } catch (error) {
