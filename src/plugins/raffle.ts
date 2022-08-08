ca420825d2ef5c48161b15a09d16793b2c556f17
Ludwig Frank <mail@ludwigfrank.com>
2022-08-08 15:55:07 +0200

    update raffle draw times

--- src/plugins/raffle.ts
+++ src/plugins/raffle.ts
@@ -15,11 +15,11 @@ type RaffleBoardOpts = {
 const mockRaffleDrawings = [{}]
 
 const mockRaffleDraws: RaffleDraw[] = [
-  { drawDate: new Date('2022-08-08T14:00Z'), winners: [] },
-  { drawDate: new Date('2022-08-09T14:00Z'), winners: [] },
-  { drawDate: new Date('2022-08-10T14:00Z'), winners: [] },
-  { drawDate: new Date('2022-08-11T14:00Z'), winners: [] },
-  { drawDate: new Date('2022-08-12T14:00Z'), winners: [] },
+  { drawDate: new Date('2022-08-08T15:00Z'), winners: [] },
+  { drawDate: new Date('2022-08-09T15:00Z'), winners: [] },
+  { drawDate: new Date('2022-08-10T15:00Z'), winners: [] },
+  { drawDate: new Date('2022-08-11T15:00Z'), winners: [] },
+  { drawDate: new Date('2022-08-12T15:00Z'), winners: [] },
 ]
 
 const initRaffle = (state: AppStore, opts: RaffleBoardOpts) => {
